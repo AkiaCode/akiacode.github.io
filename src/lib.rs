9cf6bff67c3e13ec323bd07b5e2a2f8dc7d982c3
Akiacode <catry.me@gmail.com>
2021-09-26 20:46:50 +0900

    test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@
 // but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
 
-use seed::{prelude::*, *};
+use rand::Rng;
+use seed::{*, prelude::*};
 
 // ------ ------
 //     Init
@@ -11,7 +12,9 @@ use seed::{prelude::*, *};
 
 // `init` describes what should happen when your app started.
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model { }
+    Model {
+        url: None
+    }
 }
 
 // ------ ------
@@ -19,19 +22,53 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 // ------ ------
 
 // `Model` describes our app state.
-struct Model;
+struct Model {
+    url: Option<String>
+}
 
 // ------ ------
 //    Update
 // ------ ------
 
 // (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 // `Msg` describes the different events you can modify state with.
-enum Msg {}
+enum Msg {
+    Fetch,
+    Received(Option<String>),
+}
 
 // `update` describes how to handle each `Msg`.
-fn update(_: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Fetch => {
+            orders.skip(); // No need to rerender
+            orders.perform_cmd(async {
+                if rand::thread_rng().gen::<bool>() {
+                    let response = fetch("https://nekos.life/api/v2/img/meow").await.expect("HTTP request failed");
+
+                    if let Ok(resp_json) = response.check_status() {
+                        if let Ok(json) = resp_json.json::<serde_json::Value>().await {
+                            if let Some(url) = json.get("url") {
+                                Msg::Received(Some(url.to_string()))
+                            } else {
+                                Msg::Received(None)
+                            }
+                        } else {
+                            Msg::Received(None)
+                        }
+                    } else {
+                        Msg::Received(None)
+                    }
+                } else {
+                    Msg::Received(None)
+                }
+            });
+        }
+        Msg::Received(url) => {
+            model.url = url;
+        }
+    }
 }
 
 // ------ ------
@@ -39,11 +76,98 @@ fn update(_: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {
 // ------ ------
 
 // `view` describes what to display.
-fn view(_: &Model) -> Node<Msg> {
+fn view(model: &Model) -> Node<Msg> {
+
     div![
-        "This is a counter: test",
+        style! {
+            St::BackgroundImage => if let Some(url) = &model.url {
+                format!("url({})", url)
+            } else {
+                "none".to_string()
+            },
+            St::Position => "fixed",
+            St::Top => "0",
+            St::Left => "0",
+            St::Display => "flex",
+            St::JustifyContent => "center",
+            St::AlignItems => "center",
+            St::Width => "100%",
+            St::Height => "100%",
+            St::FlexDirection => "column",
+            St::FontFamily => "Helvetica, Arial, sans-serif",
+        },
+        div!{
+            style!{
+                St::Width => px(350),
+                St::Height => px(350),
+            },
+            h1! {
+                "👩‍💻 Welcome, I`m ",
+                a! {
+                    style! {
+                        St::Color => "#f0a3d6",
+                        St::TextDecoration => "none",
+                    },
+                    attrs! {
+                        At::Href => "https://github.com/akiacode",
+                        At::Target => "_blank",
+                    },
+                    "AkiaCode"
+                }
+            },
+            h3!{
+                style!{
+                    St::MarginLeft => px(8),
+                },
+                "My Projects",
+           },
+           ul! {
+               style! {
+                    St::ListStyleType => "circle",
+                    St::Color => "#f0a3d6",
+                },
+                li!["Harmony"],
+                li!["Other..."],
+            },
+            h3 ! {
+                style!{
+                    St::MarginLeft => px(8),
+                },
+                "My Skills",
+            },
+            ul! {
+                style! {
+                    St::ListStyleType => "circle",
+                    St::Color => "#f0a3d6",
+                },
+                li!["Rust"],
+                li!["JavaScript"],
+                li!["TypeScript"],
+                li!["Python"],
+                li!["C"],
+                li!["Other..."],
+            },
+            h5! {
+                style! {
+                    St::MarginLeft => px(8),
+                },
+                "Made with ❤️ by ",
+                a! {
+                    style! {
+                        St::Color => "#f0a3d6",
+                        St::TextDecoration => "none",
+                    },
+                    attrs! {
+                        At::Href => "https://github.com/akiacode",
+                        At::Target => "_blank",
+                    },
+                    "AkiaCode"
+                }
+            }
+        }
     ]
 }
+
 
 // ------ ------
 //     Start

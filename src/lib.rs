7f79f5ce521f22a00cffb3e69c06f4484858c5af
Akiacode <catry.me@gmail.com>
2021-09-26 11:57:20 +0900

    test

--- src/lib.rs
+++ src/lib.rs
@@ -46,11 +46,9 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // ------ ------
 
 // `view` describes what to display.
-fn view(model: &Model) -> Node<Msg> {
+fn view(_model: &Model) -> Node<Msg> {
     div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model.counter, ev(Ev::Click, |_| Msg::Increment),],
+        "This is a counter: test",
     ]
 }
 
